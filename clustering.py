4018aa1cbd26eec1fa8cdcb03a384e6fc2e90f43
fbr1 <fbr1@users.noreply.github.com>
2015-10-22 23:38:17 -0300

    Agregado grafico Silhouette

--- clustering.py
+++ clustering.py
@@ -1,10 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+import matplotlib.cm as cm
 
 def EAC(D, N=45, low = 5,high = 30):
 	"""Do Evidence Accumulation Clustering
 	D= Matriz de distancia
 	N= Numero de clusterings
 	kinterval= Intervalo del cual elejir los k cluster aleatorios	
+	Devuelve matriz de coasociacion
 	"""		
 	
 	# Obtener dimensiones
@@ -14,7 +18,7 @@ def EAC(D, N=45, low = 5,high = 30):
 	
 	for i in range(N):		
 		# Elije valor aleatorio k entre low y high
-		if(low!=high):
+		if(high!=None):
 			k = np.random.randint(low,high)
 		else: 
 			k = low
@@ -30,11 +34,10 @@ def EAC(D, N=45, low = 5,high = 30):
 					if(row!=column):
 						matrizCoAsoc[row][column] = matrizCoAsoc[row][column] + 1/N
 	
-	return matrizCoAsoc
-	
+	return matrizCoAsoc	
 	
 
-def PAM(D, k, tmax = 100):	
+def PAM(D, k, return_labels=False,tmax = 100):	
 	"""Do Partitioning Around Medoids
 	D= Matriz de distancia
 	k= Numero de clusters
@@ -51,15 +54,17 @@ def PAM(D, k, tmax = 100):
 	
 	Clusters = {}
 	
+	labels = []
+	
 	# Itera hasta tmax veces o hasta que converja
 	for t in range(tmax):
 		
 		# Devuelve un array de tamaño n, donde el valor de cada uno es el índice del medioide mas cercano
-		J= np.argmin(D[:,IndicesMedioides], axis = 1)		
+		labels= np.argmin(D[:,IndicesMedioides], axis = 1)		
 		
-		# Utiliza los valores en J para reorganizarlos en k clusters		
+		# Utiliza los valores en labels para reorganizarlos en k clusters		
 		for i in range(k):
-			Clusters[i] = np.where(J==i)[0]
+			Clusters[i] = np.where(labels==i)[0]
 		
 		# Actualiza los mediodes si el costo promedio de un elemento es menor que el existente
 		for i in range(k):			
@@ -82,9 +87,63 @@ def PAM(D, k, tmax = 100):
 		
 	else:
 		# Actualiza los clusters por ultima vez
-		J= np.argmin(D[:,IndicesMedioides], axis = 1)		
+		labels = np.argmin(D[:,IndicesMedioides], axis = 1)			
 		for i in range(k):
-			Clusters[i] = np.where(J==i)[0]		
+			Clusters[i] = np.where(labels==i)[0]		
 	
+	if(return_labels):		
+		return IndicesMedioides,Clusters,labels	
+		
 	return IndicesMedioides,Clusters	
+
+def Silhouette(D,labels,k):		
+	"""
+	Taken from SKlearn's plot kmeans example	
+	D = matriz de distancia	
+	k = numero de clusters
+	"""
+	plt.ion()
+	fig, ax1 = plt.subplots()
+	fig.set_size_inches(18, 7)
+	ax1.set_xlim([-0.1, 1])
+	ax1.set_ylim([0, len(D) + (k + 1) * 10])
+	
+	sample_silhouette_values = metrics.silhouette_samples(D , labels, metric='precomputed')
+	
+	y_lower = 10
+	
+	for i in range(k):
+		ith_cluster_silhouette_values = \
+			sample_silhouette_values[labels == i]
+
+		ith_cluster_silhouette_values.sort()
+
+		size_cluster_i = ith_cluster_silhouette_values.shape[0]
+		y_upper = y_lower + size_cluster_i
+
+		color = cm.spectral(float(i) / k)
+		ax1.fill_betweenx(np.arange(y_lower, y_upper),
+						0, ith_cluster_silhouette_values,
+						facecolor=color, edgecolor=color, alpha=0.7)
+
+		ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+		
+		y_lower = y_upper + 10  
+	
+	ax1.set_title("The silhouette plot for the various clusters.")
+	ax1.set_xlabel("The silhouette coefficient values")
+	ax1.set_ylabel("Cluster label")
+
+	silhouette_avg = metrics.silhouette_score(D , labels, metric='precomputed')	
+	
+	ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+	ax1.set_yticks([]) 
+	ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+	
+	plt.suptitle(("Silhouette analysis with n_clusters =",k," and average = ",silhouette_avg),
+	fontsize=14, fontweight='bold')
+
+	plt.show()
 	
--- main.py
+++ main.py
@@ -3,49 +3,70 @@ import scipy.cluster.hierarchy as hr
 import scipy.spatial as spa
 import clustering
 import matplotlib.pyplot as plt
-import sklearn.metrics as metrics
 from sklearn.cluster import AgglomerativeClustering
 
 class cluster:	
-	def __init__(self,k):
+	def __init__(self,k,N=45,low=5,high=30):
+		self.k = k	
+		
 		# Leer datos desde archivo [Temporal]
 		data = np.genfromtxt('iris.data',delimiter=',')
 		temp= spa.distance.pdist(data,'euclidean')
-		D = spa.distance.squareform(temp)			
-		#
-		m,n = D.shape
-		coasocMatrix = clustering.EAC(D,100)
-		self.distanceMatrix = np.ones(n) - coasocMatrix	
-		#	
+		self.D = spa.distance.squareform(temp)	
 		
-		self.x = hr.linkage(self.distanceMatrix,method='complete')
-		self.z = AgglomerativeClustering(n_clusters=k, linkage='ward').fit(self.distanceMatrix)
-		self.nodes = hr.fcluster(self.x, 4,'distance') - np.ones(n,dtype=np.int8)
-		print(self.nodes)
-		print(self.z.labels_)
-		print(self.getSilluete())
-		plt.ion()		
-	def getDendogram(self):
-		hr.dendrogram(self.x)
-		plt.show()	
-	def getSilhouette(self):
-		return metrics.silhouette_score(self.distanceMatrix , self.nodes, metric='precomputed')
+		# Calcula la matriz de coasociacion
+		self.loadEAC(N,low,high)
+					
+		
 	
-
-	def getPrecision(self):			
-		originalClusters = np.genfromtxt('orCL.data',delimiter=',',dtype=None)
-		print (originalClusters)
-		print(self.nodes)
-		J =self.z.labels_	
-		print (J)
-		Clusters = {}	
-		target_names = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-		#print(metrics.classification_report(originalClusters, J, target_names=target_names))     
+	def loadEAC(self,N=45,low=5,high=30):
+		"""
+		Genera de vuelta la matriz de coasociacion
+		"""
+		m,n = self.D.shape
+		coasocMatrix = clustering.EAC(self.D,N,low,high)
+		self.EAC_D = np.ones(n) - coasocMatrix		
 		
-		for i in range(np.amin(J),np.amax(J)):
-			print(i)
-			a = np.where(J==i)
-			Clusters[i] = a
-		print(Clusters) 	   
+	def startPAM(self):
+		"""
+		Hace sobre PAM sobre la matriz de distancia del EAC
+		"""		
+		(a,b,self.labels) = clustering.PAM(self.EAC_D, self.k,True)	
+		return self.labels
+	
+	def startHierarchical(self):
+		"""
+		Hace clustering Jerarquico sobre la matriz de distancia del EAC
+		"""	
+		z = AgglomerativeClustering(n_clusters=self.k, linkage='ward').fit(self.EAC_D)
+		self.labels = z.labels_	
+		return self.labels
 
-
+	def getPrecisionIris(self):		
+		"""
+		Metodo de prueba
+		Calcula una precision de acierto. No es fiable. 
+		"""
+		
+		#Lee los cluster originales
+		originalClusters = np.genfromtxt('orCL.data',delimiter=',',dtype=None)
+		
+		results ={}		
+		
+		j=0
+		for i in range(50,151,50):
+			# Encuentra el cluster con mayor frecuencia
+			unique, counts = np.unique(self.labels[i-50:i], return_counts=True)
+			print(unique)
+			print(counts)
+			maxvalue = np.amax(counts)
+			results[j]=maxvalue/50
+			j=j+1
+			
+		print("Setosa= " + '%.2f' % results[0] + "\nVersicolor= " + '%.2f' % results[1] + "\nVirginica= " + '%.2f' % results[2])		
+			
+	def getSilhouette(self):
+		"""
+		Grafica silhouette
+		"""
+		clustering.Silhouette(self.D,self.labels,self.k)
